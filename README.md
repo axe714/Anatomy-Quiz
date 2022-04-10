c01e70717c8814f49700ea93a4517e9e62131729
Axe714 <axearzadon@gmail.com>
2022-04-10 15:40:32 -0700

    Initial commit. README updated.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CodingQuiz
+# Anatomy-Quiz
+
+This Anatomy Quiz is created to test the users knowledge on the basic anatomy/physiology of the human body. Once the 'start' button is clicked, the quiz will commence. The quiz will start with a 75-second timer. Each wrong answer will penalize the user -15 seconds to their total timer. In the end, the amount of time left on the clock will represent the score that the user receives on their quiz. 
+
+A highscore window will appear at the end where the user(s) can log their name along with the score that they have received during the quiz.
---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Which of the following is the largest organ in the body?:",
+    choices: ["Heart", "Brain", "Skin", "Lungs"],
+    answer: "Skin"
+  },
+  {
+    title: "Which mineral is responsible for the formation of our bones?",
+    choices: ["Calcium", "Pottasium", "Magnesium", "Iron"],
+    answer: "Calcium"
+  },
+  {
+    title: "Higher mental activities are controlled by which part of the brain?",
+    choices: [
+      "Parietal Lobe",
+      "Frontal Lobe",
+      "Temporal Lobe",
+      "Occipital Lobe"
+    ],
+    answer: "Frontal Lobe"
+  },
+  {
+    title:
+      "What is the largest bone in the body?",
+    choices: ["Femur", "Humerus", "Femur", "Skull"],
+    answer: "Femur"
+  },
+  {
+    title:
+      "Which blood type is considered the 'universal' blood type?",
+    choices: ["O+", "O-", "AB-", "AB+"],
+    answer: "O-"
+  },
+  {
+    title:
+      "Which of the following allows air to be passed into the lungs?",
+      choices: ["Esophagus", "Aorta", "Pulmonary Artery", "Trachea"],
+      answer: "Trachea"
+  },
+  {
+    title:
+      "Which of the following closes and seals off the lower airway during swallowing?",
+    choices: ["Alveoli", "Larynx", "Pharynx", "Epiglottis"],
+    answer: "Epiglottis"
+  },
+  {
+    title: 
+      "Which of the following best describes the process whereby the stomach muscles contract to propel food through the digestive tract?",
+      choices: ["Absorbtion", "Peristalsis", "Regurtition", "Emulsion"],
+      answer: "Peristalsis"
+  }
+];
