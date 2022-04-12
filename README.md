d93b6683c9c03586ea23c22c013782f692da2d64
Axe714 <axearzadon@gmail.com>
2022-04-11 21:59:25 -0700

    Updated README, added to showQuestions function to display current questions

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Anatomy-Quiz
 
-This Anatomy Quiz is created to test the user's knowledge on the basic anatomy/physiology of the human body. Once the 'start' button is clicked, the quiz will commence. The quiz will start with a 75-second timer. Each wrong answer will penalize the user -15 seconds to their total timer. In the end, the amount of time left on the clock will represent the score that the user receives on their quiz. 
+This Anatomy Quiz is created to test the user's knowledge on the basic anatomy/physiology of the human body. Once the 'start' button is clicked, the quiz will commence. The quiz will start with a 120 seconds on the clock (15 seconds per question). Each wrong answer will penalize the user -15 seconds to their total timer. 
 
-A highscore window will appear at the end where the user(s) can log their name along with the score that they have received during the quiz.
+In the end, the amount of time left on the clock will represent the score that the user receives on their quiz. 
+
+A highscore window will appear at the end where the user can log their name along with the score that they have received during the quiz.
--- assets/js/newlogic.js
+++ assets/js/newlogic.js
@@ -21,12 +21,11 @@ function startTimer() {
     var timer = setInterval(function () {
         //decreases timer each second
         timeLeft--;
-        //display the time remaining
+        //display the time remaining on HTML page
         timerElement.textContent = timeLeft;
+
         if (timeLeft === 0) {
             clearInterval(timer);
-            //displays time remaining on HTML
-            timerElement.innerHTML = timeLeft;
         }
         //decreases timer at a 1 second interval
     }, 1000);
@@ -35,10 +34,25 @@ function startTimer() {
 
 //showQuestions function [INCOMPLETE - to be added to the startQuiz function]
 function showQuestions () {
+
+    //sets the current question to the first question in the array
     var currentQuestionIndex = 0;
     var currentQuestion = questions[currentQuestionIndex];
-    var titleElement = document.getElementById("question-title");
+    var titleElement = document.querySelector("#question-title");
     titleElement.textContent = currentQuestion.title;
+
+    //for loop to iterate through the choices array in the current question object
+    var currentChoices = currentQuestion.choices;
+    var choicesElement = document.querySelector("#choices");
+    for (var i = 0; i < currentChoices.length; i++) {
+        var choice = currentChoices[i];
+        //creates a button for each choice in the current question
+        var choiceElementButton = document.createElement("button");
+        choiceElementButton.setAttribute("class", "choice");
+        choiceElementButton.setAttribute("value", choice);
+        choiceElementButton.textContent = i + 1 + ". " + choice;
+        choicesElement.appendChild(choiceElementButton);
+    }
 }
 
 
