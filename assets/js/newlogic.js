b23be6e620d508e6e1f8d301f498de385c054f10
Axe <axearzadon@gmail.com>
2022-04-11 19:45:45 -0700

    Added timer & displays question upon clicking 'start' quiz button.

--- assets/js/newlogic.js
+++ assets/js/newlogic.js
@@ -1,24 +1,46 @@
 //global scope variables DOM elements
-var timerElement = document.querySelector("#time");
+var timerElement = document.querySelector("#time-left");
 var startButton = document.querySelector("#start");
 var questionsElement = document.querySelector("#questions");
-var endScreenElement = document.querySelector("#end-screen");
-var endScoreElement = document.querySelector("#end-score");
-var endTimeElement = document.querySelector("#end-time");
 var endButton = document.querySelector("#end-button");
-var submitButton = document.querySelector("#submit");
 var startScreenElement = document.querySelector("#start-screen");
-var hideElements = document.querySelectorAll(".hide");
-
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
     //hides the start screen
     startScreenElement.setAttribute("class", "hide");
     questionsElement.removeAttribute("class", "hide");
-    timerElement.textContent = time;
+    startTimer();
     showQuestions();
 }
+
+//GOOD TO GO - WORKING AS INTENDED
+function startTimer() {
+    //set the timer to 15 seconds per question
+    var timeLeft = questions.length * 15;
+    var timer = setInterval(function () {
+        //decreases timer each second
+        timeLeft--;
+        //display the time remaining
+        timerElement.textContent = timeLeft;
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            //displays time remaining on HTML
+            timerElement.innerHTML = timeLeft;
+        }
+        //decreases timer at a 1 second interval
+    }, 1000);
+}
+
+
+//showQuestions function [INCOMPLETE - to be added to the startQuiz function]
+function showQuestions () {
+    var currentQuestionIndex = 0;
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleElement = document.getElementById("question-title");
+    titleElement.textContent = currentQuestion.title;
+}
+
 
 
 
