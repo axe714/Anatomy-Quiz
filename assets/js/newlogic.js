427fafb6f23d1b886d26be9ee3e0906db651401c
Axe <axearzadon@gmail.com>
2022-04-16 00:05:31 -0700

    fixed showQuestions function

--- assets/js/newlogic.js
+++ assets/js/newlogic.js
@@ -5,11 +5,13 @@ var timeLeft = questions.length * 15;
 var timerElement = document.querySelector("#time-left");
 var startButton = document.querySelector("#start");
 var questionsElement = document.querySelector("#questions");
-var endButton = document.querySelector("#end-button");
+var endScreenElement = document.querySelector("#end-screen");
 var startScreenElement = document.querySelector("#start-screen");
 var currentQuestionIndex = 0;
-startButton.addEventListener("click", startQuiz);
+var choicesBodyElement = document.querySelector("#choices");
 
+
+startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
     //hides the start screen
@@ -39,15 +41,14 @@ function startTimer() {
 
 //showQuestions function [INCOMPLETE - to be added to the startQuiz function]
 function showQuestions() {
+    choicesBodyElement.innerHTML = "";
     //sets the current question to the first question in the array
     var currentQuestion = questions[currentQuestionIndex];
-    var questionElement = document.querySelector("#questions");
     var titleElement = document.querySelector("#question-title");
     titleElement.textContent = currentQuestion.title;
 
     //for loop to iterate through the choices array in the current question object
     var currentChoices = currentQuestion.choices;
-    var choicesElement = document.querySelector("#choices");
     for (var i = 0; i < currentChoices.length; i++) {
         var choice = currentChoices[i];
         //creates a button for each choice in the current question
@@ -55,24 +56,44 @@ function showQuestions() {
         choiceElementButton.setAttribute("class", "choice");
         choiceElementButton.setAttribute("value", choice);
         choiceElementButton.textContent = i + 1 + ". " + choice;
-        choicesElement.appendChild(choiceElementButton);
+        choicesBodyElement.appendChild(choiceElementButton);
         checkAnswer();
 
         function checkAnswer() {
             choiceElementButton.onclick = function () {
                 if (this.value === currentQuestion.answer) {
-                    nextQuestion();
+                    correctAnswer();
                 } else {
                     timeLeft = timeLeft - 15;
+                    incorrectAnswer();
                 }
             }
         }
     }
 
-    function nextQuestion() {
-        questionElement.innerHTML = "";
-        currentQuestionIndex++
+    function correctAnswer() {
+        currentQuestionIndex++;
         showQuestions();
     }
+
+    function incorrectAnswer() {
+        currentQuestionIndex++;
+        showQuestions();
+    }
+
+    if (currentQuestion.length === currentQuestionIndex) {
+        quizEnd();
+
+    }
+
+    function quizEnd() {
+        var finalScoreElement = document.querySelector("#final-score");
+        questionsElement.setAttribute("class", "hide");
+        endScreenElement.removeAttribute("class", "hide");
+        finalScoreElement.textContent = timeLeft;
+
+
+    }
 }
+
 
